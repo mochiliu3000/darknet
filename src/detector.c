8f397a5849e125781be5279d11bc7fca31a8adbb
sleepywyn <sleepywyn@gmail.com>
2017-05-22 22:52:06 +0800

    fixed a bug for batch prediction. For single prediction, uncomment source code is required

--- src/detector.c
+++ src/detector.c
@@ -580,7 +580,7 @@ void validate_detector_recall(char *cfgfile, char *weightfile)
     }
 }
 
-void test_detector(char *datacfg, char *cfgfile, char *weightfile, char *filename, float thresh, float hier_thresh, char *outfile, int fullscreen)
+void test_detector(char *datacfg, char *cfgfile, char *weightfile, char **file_names, float thresh, float hier_thresh, char *outfile, int fullscreen)
 {
     list *options = read_data_cfg(datacfg);
     char *name_list = option_find_str(options, "names", "data/names.list");
@@ -635,21 +635,21 @@ void test_detector(char *datacfg, char *cfgfile, char *weightfile, char *filenam
         if (nms) do_nms_obj(boxes, probs, l.w*l.h*l.n, l.classes, nms);
         //else if (nms) do_nms_sort(boxes, probs, l.w*l.h*l.n, l.classes, nms);
         draw_detections(im, l.w*l.h*l.n, thresh, boxes, probs, names, alphabet, l.classes);
-        if(outfile){
-            save_image(im, outfile);
-        }
-        else{
-            save_image(im, "predictions");
-#ifdef OPENCV
-            cvNamedWindow("predictions", CV_WINDOW_NORMAL); 
-            if(fullscreen){
-                cvSetWindowProperty("predictions", CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
-            }
-            show_image(im, "predictions");
-            cvWaitKey(0);
-            cvDestroyAllWindows();
-#endif
-        }
+//         if(outfile){
+//             save_image(im, outfile);
+//         }
+//         else{
+//             save_image(im, "predictions");
+// #ifdef OPENCV
+//             cvNamedWindow("predictions", CV_WINDOW_NORMAL); 
+//             if(fullscreen){
+//                 cvSetWindowProperty("predictions", CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
+//             }
+//             show_image(im, "predictions");
+//             cvWaitKey(0);
+//             cvDestroyAllWindows();
+// #endif
+//         }
 
         free_image(im);
         free_image(sized);
@@ -658,6 +658,86 @@ void test_detector(char *datacfg, char *cfgfile, char *weightfile, char *filenam
         //if (filename) break;
     }
 }
+
+void test_detector_for_prediction(char *datacfg, char *cfgfile, char *weightfile, char **file_names, float thresh, float hier_thresh, char *outfile, int fullscreen)
+{
+    list *options = read_data_cfg(datacfg);
+    char *name_list = option_find_str(options, "names", "data/names.list");
+    char **names = get_labels(name_list);
+
+    image **alphabet = load_alphabet();
+    network net = parse_network_cfg(cfgfile);
+    if(weightfile){
+        load_weights(&net, weightfile);
+    }
+    set_batch_network(&net, 1);
+    srand(2222222);
+    clock_t time;
+    char buff[256];
+    char *input = buff;
+    int j;
+    float nms=.4;
+
+    // int file_num = sizeof(file_names) / sizeof(file_names[0]);
+    // for(int i=0; i < file_num; i++){
+    //     char* filename = file_names[i];
+    int namesLen = -1;
+    while (file_names[++namesLen] != NULL) {
+        char* filename = file_names[namesLen];
+    // while(1){
+        if(filename){
+            strncpy(input, filename, 256);
+        } else {
+            printf("Enter Image Path: ");
+            fflush(stdout);
+            input = fgets(input, 256, stdin);
+            if(!input) return;
+            strtok(input, "\n");
+        }
+        image im = load_image_color(input,0,0);
+        image sized = letterbox_image(im, net.w, net.h);
+        //image sized = resize_image(im, net.w, net.h);
+        //image sized2 = resize_max(im, net.w);
+        //image sized = crop_image(sized2, -((net.w - sized2.w)/2), -((net.h - sized2.h)/2), net.w, net.h);
+        //resize_network(&net, sized.w, sized.h);
+        layer l = net.layers[net.n-1];
+
+        box *boxes = calloc(l.w*l.h*l.n, sizeof(box));
+        float **probs = calloc(l.w*l.h*l.n, sizeof(float *));
+        for(j = 0; j < l.w*l.h*l.n; ++j) probs[j] = calloc(l.classes + 1, sizeof(float *));
+
+        float *X = sized.data;
+        time=clock();
+        network_predict(net, X);
+        printf("%s: Predicted in %f seconds.\n", input, sec(clock()-time));
+        get_region_boxes(l, im.w, im.h, net.w, net.h, thresh, probs, boxes, 0, 0, hier_thresh, 1);
+        if (nms) do_nms_obj(boxes, probs, l.w*l.h*l.n, l.classes, nms);
+        //else if (nms) do_nms_sort(boxes, probs, l.w*l.h*l.n, l.classes, nms);
+        draw_detections(im, l.w*l.h*l.n, thresh, boxes, probs, names, alphabet, l.classes);
+//         if(outfile){
+//             save_image(im, outfile);
+//         }
+//         else{
+//             save_image(im, "predictions");
+// #ifdef OPENCV
+//             cvNamedWindow("predictions", CV_WINDOW_NORMAL); 
+//             if(fullscreen){
+//                 cvSetWindowProperty("predictions", CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
+//             }
+//             show_image(im, "predictions");
+//             cvWaitKey(0);
+//             cvDestroyAllWindows();
+// #endif
+//         }
+
+        free_image(im);
+        free_image(sized);
+        free(boxes);
+        free_ptrs((void **)probs, l.w*l.h*l.n);
+        //if (filename) break;
+    }
+}
+
 
 void run_detector(int argc, char **argv)
 {
